4fbbf533350cb9bbcece4e6c9cd018e6ef64401c
SIGMazer <mazinasd7@gmail.com>
2023-05-22 16:14:56 +0300

    change files name

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Step 1: Create the necessary directories
-mkdir -p ~/.config/nvim/rplugin/python
-
-# Step 2: Copy the Python plugin files
-cp  ./auto-git/auto_git.py ~/.config/nvim/rplugin/python/auto-git.py
-
-
